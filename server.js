0a8111432eab3c9d3a999e9835e23e6b6509e15a
kperusko <kristijan.perusko@yahoo.com>
2015-01-20 12:26:39 -0500

    Added app error handlers

--- server.js
+++ server.js
@@ -25,5 +25,34 @@ app.use(function(req, res, next) {
 var apiRoutes = require('./routes/api')(app, express);
 app.use('/api', apiRoutes);
 
+// connection errors
+mongoose.connection.on('error',  console.error.bind(console, 'connection error:'));
+
+
+// 404 - catch and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// 500 - development error handler; print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500).send({
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// 500 - production error handler; no stacktrace
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500).send({
+        message: err.message,
+        error: {}
+    });
+});
+
 app.listen(config.http.port);
 console.log('Listening on port ' + config.http.port);
