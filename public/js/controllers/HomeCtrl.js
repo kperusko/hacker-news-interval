74612912684bc1035a38061d3c22e8167a669b75
kperusko <kristijan.perusko@yahoo.com>
2015-01-27 14:26:43 -0500

    Added chart with mock data

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,4 +1,43 @@
+angular.module('homeController', ['chart.js', 'ui.bootstrap']).controller('HomeCtrl',
+  function ($scope, $http) {
+	var chart = {};
 
-angular.module('homeController', []).controller('HomeCtrl', function($scope) {
+	var normalizeDate = function(date) {
+		date = new Date(date);
+		date.setMinutes (date.getMinutes() + 30);
+		date.setMinutes (0);
+		return date.toLocaleTimeString();
+	};
 
+	$http.get('api/snapshots').success(function(data) {
+		var chartLabels = [],
+      		chartData = []; 
+	
+		data.forEach(function(snapshot){
+			chartLabels.push(normalizeDate(snapshot.time));
+			chartData.push(snapshot.new_items);
+		});
+
+		console.log(chartLabels);
+	});
+
+	$scope.labels = [];
+    $scope.series = ['New stories', 'Bla'];
+    $scope.chartData = [
+		[28, 48, 40, 19, 86, 27, 90],
+
+		[60, 20, 30, 50]
+    ];
+    $scope.options = { 
+		bezierCurve: false,
+		animationSteps: 10, 
+		responsive: true,
+	};
+
+	$scope.onClick = function (points, evt) {
+		$scope.chartData = [[22, 23, 43]];
+	};
+
+	$scope.totalItems = 50;
+	$scope.currentPage = 2;
 });
