9cf0b63dd8d0f2fe18f85559ad6a9f1db226b94f
kperusko <kristijan.perusko@yahoo.com>
2015-01-27 19:20:42 -0500

    Added basic table with mock data

--- public/js/controllers/HomeCtrl.js
+++ public/js/controllers/HomeCtrl.js
@@ -1,6 +1,6 @@
-angular.module('homeController', ['chart.js', 'ui.bootstrap']).controller(
+angular.module('homeController', ['chart.js', 'ui.bootstrap', 'ngTable']).controller(
   'HomeCtrl',
-  function ($scope, $http) {
+  function ($scope, $http, $filter, ngTableParams) {
     var normalizeDate = function (date) {
       date = new Date(date);
       return date.toLocaleTimeString();
@@ -13,7 +13,7 @@ angular.module('homeController', ['chart.js', 'ui.bootstrap']).controller(
       options: {
         bezierCurve: false,
         animationSteps: 10,
-        responsive: true,
+        //        responsive: true,
       },
       currentPage: 1,
       totalItems: 0,
@@ -42,4 +42,36 @@ angular.module('homeController', ['chart.js', 'ui.bootstrap']).controller(
       $scope.chart.data = [chartData.slice(begin, end)];
       $scope.chart.labels = chartLabels.slice(begin, end);
     });
+
+    var stories = [{
+      title: 'Big story',
+      rank: 1,
+      score: 50
+    }, {
+      title: 'Huge',
+      rank: 2,
+      score: 43
+    }];
+
+    $scope.tableParams = new ngTableParams({
+      page: 1, // show first page
+      count: 50, // count per page
+      sorting: {
+        rank: 'asc' // initial sorting
+      }
+    }, {
+      counts: [], // hide page counts control
+      total: 1, // must be lower than count to hide pagination
+      getData: function ($defer, params) {
+
+        // use build-in angular filter
+        var orderedData = params.sorting() ?
+          $filter('orderBy')(stories, params.orderBy()) :
+          stories;
+
+        $defer.resolve(orderedData.slice((params.page() - 1) * params
+          .count(), params.page() * params.count()));
+      }
+    });
+
   });
