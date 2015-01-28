d27dbd7952d2de73702e9333200b2c6ef3b0c110
kperusko <kristijan.perusko@yahoo.com>
2015-01-28 12:55:28 -0500

    Cleanup and renamed HomeCtrl to homeCtrl

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,53 +1,57 @@
-angular.module('homeController',
-			   ['chart.js', 'ui.bootstrap', 'ngTable', 'intervalService'
-]).controller('HomeCtrl', 
+angular.module('homeController', ['chart.js', 'ui.bootstrap', 'ngTable',
+  'intervalService'
+]).controller('HomeCtrl', ['$scope', '$http', '$filter', 'Snapshot', 'ngTableParams',
   function ($scope, $http, $filter, Snapshot, ngTableParams) {
-	var vm = this;
+    var vm = this;
 
-	// Chart data
+    // Chart data and settings
     $scope.chart = {
       data: {
-		  labels: [],
-		  values: []
-	  },
-	  // Chart settings
+        labels: [],
+        values: []
+      },
+      // Chart settings
       series: ['New stories'],
       options: {
         bezierCurve: false,
         animationSteps: 10
       },
-	  // Pagination settings
+      // Pagination settings
       currentPage: 1,
       totalItems: 0,
       itemsPerPage: 8
     };
 
-	vm.allSnapshots = {
-		labels: [],
-		values: []
-	};
+	// stores all snapshots that are later
+	// sliced for pagination
+    vm.allSnapshots = {
+      labels: [],
+      values: []
+    };
 
     // Load data for chart
     Snapshot.getSnapshots()
-	  .then(function (snapshots) {
-	  vm.allSnapshots = snapshots;
-      // Set the total items for the paginator
-      $scope.chart.totalItems = vm.allSnapshots.values.length;
+      .then(function (snapshots) {
+        vm.allSnapshots = snapshots;
+        // Set the total items for the paginator
+        $scope.chart.totalItems = vm.allSnapshots.values.length;
 
-      // Go to last page 
-      // This triggers the chart reloading and displaying data
-      $scope.chart.currentPage =
-        Math.ceil($scope.chart.totalItems / $scope.chart.itemsPerPage);
-    });
+        // Go to last page 
+        // This triggers the chart reloading and displaying data
+        $scope.chart.currentPage =
+          Math.ceil($scope.chart.totalItems / $scope.chart.itemsPerPage);
+      });
 
-	// React to paginator prev/next buttons
+    // Watch for paginator prev/next actions
     $scope.$watch('chart.currentPage', function () {
       var begin = (($scope.chart.currentPage - 1) * $scope.chart.itemsPerPage),
         end = begin + $scope.chart.itemsPerPage;
 
-	  // Get the chart data for the current page
-      $scope.chart.data.values = [vm.allSnapshots.values.slice(begin, end)];
-      $scope.chart.data.labels = vm.allSnapshots.labels.slice(begin, end);
+      // Get the chart data for the current page
+      $scope.chart.data.values = 
+			[vm.allSnapshots.values.slice(begin, end)];
+      $scope.chart.data.labels = 
+			vm.allSnapshots.labels.slice(begin, end);
     });
 
     var stories = [{
@@ -62,7 +66,7 @@ angular.module('homeController',
       url: 'http://google.com'
     }];
 
-	// Set up table options
+    // Set up table options
     $scope.tableParams = new ngTableParams({
       page: 1, // show first page
       count: 50, // count per page
@@ -83,4 +87,5 @@ angular.module('homeController',
           .count(), params.page() * params.count()));
       }
     });
-  });
+  }
+]);
--- public/js/services/snapshots.js
+++ public/js/services/snapshots.js
@@ -4,33 +4,32 @@ intervalServices.factory('Snapshot', ['$http', '$q',
   function ($http, $q) {
     var service = {};
 
+    service.getSnapshots = function () {
+      function normalizeDate(date) {
+        date = new Date(date);
+        return date.toLocaleTimeString();
+      }
 
-	service.getSnapshots = function() {
-		function normalizeDate(date) {
-			date = new Date(date);
-			return date.toLocaleTimeString();
-		}
+      var deferred = $q.defer();
+      $http.get('api/snapshots')
+        .success(function (data) {
+          var snapshots = {
+            labels: [],
+            values: []
+          };
 
-		var deferred = $q.defer();
-		$http.get('api/snapshots')
-			.success(function(data){
-				var snapshots = {
-					labels: [],
-					values: []
-				};
-				
-				data.forEach(function (snapshot) {
-					snapshots.labels.push(normalizeDate(snapshot.time));
-					snapshots.values.push(snapshot.new_items);
-				});
-				deferred.resolve(snapshots);
-			})
-		    .error(function(){
-				deferred.reject('There was an error retrieving snapshots');
-			});
-		return deferred.promise;
-	};
-	
-	return service;
+          data.forEach(function (snapshot) {
+            snapshots.labels.push(normalizeDate(snapshot.time));
+            snapshots.values.push(snapshot.new_items);
+          });
+          deferred.resolve(snapshots);
+        })
+        .error(function () {
+          deferred.reject('There was an error retrieving snapshots');
+        });
+      return deferred.promise;
+    };
+
+    return service;
   }
 ]);
